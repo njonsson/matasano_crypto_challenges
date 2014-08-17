783674dd3f17ae01a2d7b21f88132881577dd668
Nils Jonsson <nils@alumni.rice.edu>
2014-08-16 22:21:36 -0500

    Refactor namespace 'require' logic to DRY

--- lib/matasano_crypto_challenges.rb
+++ lib/matasano_crypto_challenges.rb
@@ -1,6 +1,17 @@
-pattern = /^#{Regexp.escape File.expand_path('..', __FILE__)}\/(.+)\.rb$/
-Dir.glob(File.expand_path("../#{File.basename __FILE__, '.rb'}/*.rb", __FILE__ )) do |f|
-  require f.gsub(pattern, '\1')
+def require_contained(&block)
+  options = Struct.new(:level).new(1)
+  block.call options
+  pattern_relative = (['..'] * options.level).join('/')
+  filename = block.binding.eval('__FILE__')
+  pattern = /^#{Regexp.escape File.expand_path(pattern_relative, filename)}\/(.+)\.rb$/
+  Dir.glob(File.expand_path("../#{File.basename filename, '.rb'}/*.rb",
+           filename)) do |f|
+    require f.gsub(pattern, '\1')
+  end
+end
+
+require_contained do |options|
+  options.level = 1
 end
 
 module MatasanoCryptoChallenges; end
--- lib/matasano_crypto_challenges/representations.rb
+++ lib/matasano_crypto_challenges/representations.rb
@@ -1,6 +1,5 @@
-pattern = /^#{Regexp.escape File.expand_path('../..', __FILE__)}\/(.+)\.rb$/
-Dir.glob(File.expand_path("../#{File.basename __FILE__, '.rb'}/*.rb", __FILE__ )) do |f|
-  require f.gsub(pattern, '\1')
+require_contained do |options|
+  options.level = 2
 end
 
 module MatasanoCryptoChallenges
