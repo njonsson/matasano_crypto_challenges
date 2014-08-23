37dd712d91fc461b03eadc4a6406ec6fd51b6ea0
Nils Jonsson <nils@alumni.rice.edu>
2014-08-23 10:21:21 -0500

    Factor out Util.decrypt_aes128_ecb from Set01::Challenge07

--- lib/matasano_crypto_challenges/set_01/challenge_07.rb
+++ lib/matasano_crypto_challenges/set_01/challenge_07.rb
@@ -1,5 +1,5 @@
 require 'matasano_crypto_challenges/representations/base64'
-require 'openssl'
+require 'matasano_crypto_challenges/util'
 
 module MatasanoCryptoChallenges
 
@@ -9,18 +9,9 @@ module MatasanoCryptoChallenges
 
       def decrypt_aes128_ecb(ciphertext_base64_string: nil, key_string: nil)
         ciphertext_base64 = Representations::Base64.from_value(ciphertext_base64_string)
-        aes128_ecb_cipher.decrypt
-        aes128_ecb_cipher.key = key_string
-        plaintext_string = aes128_ecb_cipher.update(ciphertext_base64.string) +
-                           aes128_ecb_cipher.final
-        result = Representations::Base64.from_string(plaintext_string)
+        result = Util.decrypt_aes128_ecb(ciphertext_representation: ciphertext_base64,
+                                         key: key_string.bytes)
         result.value
-      end
-
-    private
-
-      def aes128_ecb_cipher
-        @aes128_ecb_cipher ||= OpenSSL::Cipher::AES.new(128, :ECB)
       end
 
     end
--- lib/matasano_crypto_challenges/util.rb
+++ lib/matasano_crypto_challenges/util.rb
@@ -1,10 +1,20 @@
 require 'matasano_crypto_challenges/refinements/array'
+require 'openssl'
 
 using MatasanoCryptoChallenges::Refinements::Array
 
 module MatasanoCryptoChallenges
 
   module Util
+
+    def self.decrypt_aes128_ecb(ciphertext_representation: nil, key: nil)
+      aes128_ecb_cipher = OpenSSL::Cipher::AES.new(128, :ECB)
+      aes128_ecb_cipher.decrypt
+      aes128_ecb_cipher.key = Array(key).collect(&:chr).join
+      plaintext_string = aes128_ecb_cipher.update(ciphertext_representation.string) +
+                         aes128_ecb_cipher.final
+      ciphertext_representation.class.from_string plaintext_string
+    end
 
     def self.xor_with_repeating_key(content_representation: nil, key: nil)
       repeated_key_representation = content_representation.class.from_bytes(Array(key).repeat_to(content_representation.bytes.length))
